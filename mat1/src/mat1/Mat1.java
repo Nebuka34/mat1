66e982d0b48357ef1a4914cce5248406a59b8e6d
Girişyok <Giriyok@Girisyok>
2019-10-08 20:00:10 +0300

    matematik toplama carpma bölme

---
+++ mat1/src/mat1/Mat1.java
@@ -0,0 +1,14 @@
+package mat1;
+
+public class Mat1 {
+	public static void main(String[] args) {
+
+	       // Basit Hesaplama iþlem ekraný
+	        //a=4 b=6 c=8 
+		 	int a=4;
+			int b=6;
+			int c=8;
+				
+					System.out.println (a+b-c+2*a*b*c-7);
+}
+}
